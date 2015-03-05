00bf4bfefb8dc33a68147198aed6aa10430c8bbb
Yifan Gu <yifan.gu@coreos.com>
2015-03-05 11:29:02 -0800

    Add container runtime interface and rocket runtime.

---
+++ pkg/kubelet/container.go
@@ -0,0 +1,31 @@
+/*
+Copyright 2015 CoreOS Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package kubelet
+
+import "github.com/GoogleCloudPlatform/kubernetes/pkg/api"
+
+// ContainerRuntime interface defines the interfaces that should be implemented
+// by a container runtime.
+type ContainerRuntime interface {
+	Version() (map[string]string, error)
+	ListPods() ([]*api.Pod, error)
+	RunPod(*api.BoundPod) error
+	KillPod(*api.Pod) error
+	RunContainerInPod(*api.Container, *api.Pod) error
+	RestartContainerInPod(*api.Container, *api.Pod) error
+	KillContainerInPod(*api.Container, *api.Pod) error
+}
---
+++ pkg/kubelet/rocket/rocket.go
@@ -0,0 +1,425 @@
+/*
+Copyright 2015 CoreOS Inc. All rights reserved.
+
+Licensed under the Apache License, Version 2.0 (the "License");
+you may not use this file except in compliance with the License.
+You may obtain a copy of the License at
+
+    http://www.apache.org/licenses/LICENSE-2.0
+
+Unless required by applicable law or agreed to in writing, software
+distributed under the License is distributed on an "AS IS" BASIS,
+WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+See the License for the specific language governing permissions and
+limitations under the License.
+*/
+
+package rocket
+
+import (
+	"encoding/json"
+	"fmt"
+	"io"
+	"os"
+	"os/exec"
+	"path"
+	"strings"
+	"syscall"
+
+	"github.com/GoogleCloudPlatform/kubernetes/pkg/api"
+	"github.com/coreos/go-systemd/dbus"
+	"github.com/coreos/go-systemd/unit"
+	"github.com/golang/glog"
+)
+
+const (
+	kubernetesUnitPrefix = "K8S"
+	systemdServiceDir    = "/etc/systemd/system"
+
+	unitKubernetesSection = "X-Kubernetes"
+	rocketIDKey           = "RocketID"
+	unitPodName           = "POD"
+)
+
+const (
+	rocketBinName = "rkt"
+
+	podStateEmbryo    = "embryo"
+	podStatePreparing = "preparing"
+	podStatePrepared  = "prepared"
+	podStateRunning   = "running"
+	podStateExited    = "exited"
+	podStateDeleting  = "deleting"
+	podStateGone      = "gone"
+)
+
+// RocketRuntime implements the ContainerRuntimeInterface for rocket container runtime.
+type RocketRuntime struct {
+	systemd *dbus.Conn
+	absPath string
+}
+
+// NewRocketRuntime creates the rocket container runtime with the given
+// path to the rkt binary.
+func NewRocketRuntime() (*RocketRuntime, error) {
+	systemd, err := dbus.New()
+	if err != nil {
+		return nil, err
+	}
+	// Test if 'rkt' is in $PATH.
+	absPath, err := exec.LookPath(rocketBinName)
+	if err != nil {
+		return nil, err
+	}
+	rkt := &RocketRuntime{
+		systemd: systemd,
+		absPath: absPath,
+	}
+
+	// Simply verify the binary by trying 'rkt version'.
+	result, err := rkt.Version()
+	if err != nil {
+		return nil, err
+	}
+	if _, found := result[rocketBinName]; !found {
+		return nil, fmt.Errorf("Cannot get the version of rocket")
+	}
+	glog.V(4).Infof("rocket version: %v", result)
+	return rkt, nil
+}
+
+// Version invokes 'rkt version' to get the version information of the rocket
+// runtime on the machine.
+// The return values are a map of component:version.
+//
+// Example:
+// rkt:0.3.2+git
+// appc:0.3.0+git
+//
+func (r *RocketRuntime) Version() (map[string]string, error) {
+	output, err := runCommand("version")
+	if err != nil {
+		return nil, err
+	}
+
+	// Example output for 'rkt version':
+	// rkt version 0.3.2+git
+	// appc version 0.3.0+git
+	result := make(map[string]string)
+	for _, line := range output {
+		tuples := strings.Split(strings.TrimSpace(line), " ")
+		if len(tuples) != 3 {
+			glog.Warningf("Cannot parse the output: %q", line)
+			continue
+		}
+		result[tuples[0]] = tuples[2]
+	}
+	return result, nil
+}
+
+// ListPods runs 'systemctl list-unit' and 'rkt list' to get the list of all the appcs.
+// Then it will use the result to contruct list of pods.
+func (r *RocketRuntime) ListPods() ([]*api.Pod, error) {
+	glog.V(4).Infof("rocket is listing pods")
+
+	units, err := r.systemd.ListUnits()
+	if err != nil {
+		return nil, err
+	}
+
+	var pods []*api.Pod
+	for _, u := range units {
+		if strings.HasPrefix(u.Name, kubernetesUnitPrefix) {
+			pod, err := makePod(u.Name)
+			if err != nil {
+				glog.Warningf("Cannot construct pod from unit file: %v", err)
+				continue
+			}
+			pods = append(pods, pod)
+		}
+	}
+	return pods, nil
+}
+
+// RunPod first creates the unit file for a pod, and then invokes
+// 'systemctl start' to run that unit file.
+func (r *RocketRuntime) RunPod(pod *api.BoundPod) error {
+	glog.V(4).Infof("rocket is running pod: name %q", pod.Name)
+
+	name, err := r.preparePod(pod)
+	if err != nil {
+		return err
+	}
+
+	ch := make(chan string)
+	_, err = r.systemd.StartUnit(name, "replace", ch)
+	if err != nil {
+		return err
+	}
+	if status := <-ch; status != "done" {
+		return fmt.Errorf("Unexpected return status %q", status)
+	}
+	return nil
+}
+
+// KillPod invokes 'systemctl kill' to kill the unit that runs the pod.
+func (r *RocketRuntime) KillPod(pod *api.Pod) error {
+	glog.V(4).Infof("rocket is killing pod: name %q", pod.Name)
+
+	serviceName := makePodServiceFileName(pod.Name, pod.Namespace)
+
+	// TODO(yifan): More graceful stop. Replace with StopUnit and wait for a timeout.
+	r.systemd.KillUnit(serviceName, int32(syscall.SIGKILL))
+	if err := r.systemd.ResetFailedUnit(serviceName); err != nil {
+		return err
+	}
+	return nil
+}
+
+// RunContainerInPod launches a container in the given pod.
+// For now, we need to kill and restart the whole pod. Hopefully we will be
+// launching this single container without touching its sliblings in the near future.
+func (r *RocketRuntime) RunContainerInPod(container *api.Container, pod *api.Pod) error {
+	if err := r.KillPod(pod); err != nil {
+		return err
+	}
+
+	// Update the pod and start it.
+	pod.Spec.Containers = append(pod.Spec.Containers, *container)
+	boundPod := &api.BoundPod{pod.TypeMeta, pod.ObjectMeta, pod.Spec}
+	if err := r.RunPod(boundPod); err != nil {
+		return err
+	}
+	return nil
+}
+
+// RestartContainerInPod restarts a container in the given pod.
+// Like RunContainerInPod, we need to kill and restart the whole pod for now.
+func (r *RocketRuntime) RestartContainerInPod(container *api.Container, pod *api.Pod) error {
+	if err := r.KillPod(pod); err != nil {
+		return err
+	}
+
+	// Update the pod and start it.
+	for i, c := range pod.Spec.Containers {
+		glog.V(4).Infof("found the cotnainer: %#v", c)
+		if c.Name == container.Name {
+			pod.Spec.Containers[i] = *container
+			boundPod := &api.BoundPod{pod.TypeMeta, pod.ObjectMeta, pod.Spec}
+			return r.RunPod(boundPod)
+		}
+	}
+	return fmt.Errorf("Cannot find the container: %q, in the pod: %q", container.Name, pod.Name)
+}
+
+// KillContainer kills the container in the given pod.
+// Like RunContainerInPod, we will have to tear down the whole pod first to kill this
+// single container.
+func (r *RocketRuntime) KillContainerInPod(container *api.Container, pod *api.Pod) error {
+	if err := r.KillPod(pod); err != nil {
+		return err
+	}
+
+	// Update the pod and start it.
+	var containers []api.Container
+	for _, c := range pod.Spec.Containers {
+		if c.Name == container.Name {
+			continue
+		}
+		containers = append(containers, c)
+	}
+	boundPod := &api.BoundPod{pod.TypeMeta, pod.ObjectMeta, pod.Spec}
+	return r.RunPod(boundPod)
+}
+
+// runCommand invokes 'rkt [subCommand] [args]' and returns the result from stdout
+// in a list of strings.
+func runCommand(subCommand string, args ...string) ([]string, error) {
+	glog.V(4).Info("run rkt command:", subCommand, args)
+	var allArgs []string
+	allArgs = append(allArgs, subCommand)
+	allArgs = append(allArgs, args...)
+	output, err := exec.Command(rocketBinName, allArgs...).Output()
+	if err != nil {
+		return nil, err
+	}
+	return strings.Split(strings.TrimSpace(string(output)), "\n"), nil
+}
+
+// getPodsState returns a map of [pod-uuid]:[pod-state].
+func getPodsState() (map[string]string, error) {
+	result := make(map[string]string)
+	output, err := runCommand("list", "--no-legend")
+	if err != nil {
+		return nil, err
+	}
+
+	if len(output) == 0 {
+		// No pods is running.
+		return nil, nil
+	}
+
+	// Example output of current 'rkt list' (version <= 0.4.0):
+	// UUID                                 ACI     STATE
+	// 2372bc17-47cb-43fb-8d78-20b31729feda	foo     running
+	//                                      bar
+	// 40e2813b-9d5d-4146-a817-0de92646da96 foo     exited
+	// 40e2813b-9d5d-4146-a817-0de92646da96 bar     exited
+	//
+	// With '--no-legend', the first line is eliminated.
+	for _, line := range output {
+		tuples := strings.Split(strings.TrimSpace(line), "\t")
+		if len(tuples) != 3 {
+			continue
+		}
+		result[tuples[0]] = tuples[2]
+	}
+	return result, nil
+}
+
+// getPodStatus fills the status of the given pod. Especially, it will
+// fill the status for each container in the pod.
+func getPodStatus(pod *api.Pod) error {
+	// TODO(yifan) Cache this.
+	podStates, err := getPodsState()
+	if err != nil {
+		return err
+	}
+
+	rktID, found := pod.Annotations[rocketIDKey]
+	if !found {
+		// TODO(yifan): Maybe we should panic here...
+		return fmt.Errorf("Impossible: Cannot find rocket pod: %v", pod)
+	}
+
+	state, found := podStates[rktID]
+	if !found {
+		return fmt.Errorf("Cannot find the state for pod: %q, rocket ID: %q", pod.Name, rktID)
+	}
+
+	// For now just make every container's state as same as the pod.
+	pod.Status.Info = make(map[string]api.ContainerStatus)
+	for _, container := range pod.Spec.Containers {
+		// TODO(yifan): Pull out creationg, starting time.
+		switch state {
+		case podStateRunning:
+			pod.Status.Info[container.Name] = api.ContainerStatus{
+				State: api.ContainerState{
+					Running: &api.ContainerStateRunning{},
+				},
+			}
+		case podStateEmbryo, podStatePreparing, podStatePrepared:
+			pod.Status.Info[container.Name] = api.ContainerStatus{
+				State: api.ContainerState{
+					Waiting: &api.ContainerStateWaiting{},
+				},
+			}
+		case podStateExited, podStateDeleting, podStateGone:
+			pod.Status.Info[container.Name] = api.ContainerStatus{
+				State: api.ContainerState{
+					Termination: &api.ContainerStateTerminated{},
+				},
+			}
+		default:
+			return fmt.Errorf("Unexpected state: %q", state)
+		}
+	}
+	return nil
+}
+
+// makePod constructs the pod by reading information from the given unit file
+// and from rocket APIs.
+func makePod(unitName string) (*api.Pod, error) {
+	f, err := os.Open(path.Join(systemdServiceDir, unitName))
+	if err != nil {
+		return nil, err
+	}
+	defer f.Close()
+
+	var pod api.Pod
+	opts, err := unit.Deserialize(f)
+	if err != nil {
+		return nil, err
+	}
+
+	for _, opt := range opts {
+		if opt.Section != unitKubernetesSection {
+			continue
+		}
+		if opt.Name == unitPodName {
+			// NOTE: In fact we unmarshal from a serialized
+			// api.BoundPod type here.
+			err = json.Unmarshal([]byte(opt.Value), &pod)
+			if err != nil {
+				return nil, err
+			}
+		}
+	}
+
+	if err = getPodStatus(&pod); err != nil {
+		glog.Errorf("Error getting pod status of pod %q", pod.Name)
+		return nil, err
+	}
+	return &pod, nil
+}
+
+// makePodServiceFileName constructs the unit file name for the given pod.
+// TODO(yifan), when BoundPod and Pod are combined, we can change the input to
+// take just a pod.
+func makePodServiceFileName(podName, podNamespace string) string {
+	return fmt.Sprintf("%s_%s_%s.service", kubernetesUnitPrefix, podName, podNamespace)
+}
+
+// preparePod creates the unit file and save it under systemdUnitDir.
+func (r *RocketRuntime) preparePod(pod *api.BoundPod) (string, error) {
+	// Get the pod's uuid.
+	var images []string
+	for _, c := range pod.Spec.Containers {
+		images = append(images, c.Image)
+	}
+	output, err := runCommand("prepare", images...)
+	if err != nil {
+		return "", err
+	}
+	if len(output) != 1 {
+		return "", fmt.Errorf("Cannot get uuid from 'rkt prepare'")
+	}
+
+	uuid := output[0]
+	glog.V(4).Infof("rkt prepare returns %q", uuid)
+
+	// Save the rocket uuid in pod annotations.
+	pod.Annotations[rocketIDKey] = uuid
+	b, err := json.Marshal(pod)
+	if err != nil {
+		return "", err
+	}
+
+	units := []*unit.UnitOption{
+		{
+			Section: unitKubernetesSection,
+			Name:    unitPodName,
+			Value:   string(b),
+		},
+		{
+			Section: "Service",
+			Name:    "ExecStart",
+			Value:   fmt.Sprintf("%s run-prepared %s", r.absPath, uuid),
+		},
+	}
+
+	// Save the unit file under systemd's service directory.
+	// TODO(yifan) Garbage collect 'dead' serivce files.
+	unitName := makePodServiceFileName(pod.Name, pod.Namespace)
+	unitFile, err := os.Create(path.Join(systemdServiceDir, unitName))
+	if err != nil {
+		return "", err
+	}
+	defer unitFile.Close()
+
+	_, err = io.Copy(unitFile, unit.Serialize(units))
+	if err != nil {
+		return "", err
+	}
+	return unitName, nil
+}
